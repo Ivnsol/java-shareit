2707d6b69d02a596fa0e4a8e2562e80b4fd77a99
John_SL <vansol9797@yandex.ru>
2024-03-19 09:03:11 +0300

    change equals user

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -25,17 +25,4 @@ public class User {
     @NotEmpty
     @Column(unique = true)
     private String email;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
